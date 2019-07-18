9798f574111b1b3b320d85db0a775092634505a0
Colin Cason <colinjcason@gmail.com>
2019-07-18 12:29:11 -0700

    made html outline and linked files

---
+++ javascript.js
@@ -0,0 +1,8 @@
+// declare variables to keep scores for game
+var wins = 0;
+var losses = 0;
+var totalScore = 0;
+
+// ranNumber is picking a number between 19-120
+var ranNumber = Math.round(Math.random() * 120) + 19;
+
